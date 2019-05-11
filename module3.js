80409e0f1f65f0e285d6df4893533af084a19a7a
sapatelbaps <sapatelbaps@gmail.com>
2019-05-11 13:45:19 -0700

    Module 3 to reaed file sync.

---
+++ module3.js
@@ -0,0 +1,7 @@
+// read file and show number of \n new line chars.
+var fs = require('fs');
+var contentBuffer = fs.readFileSync(process.argv[2]);
+var data = contentBuffer.toString();
+var lines = data.split('\n').length;
+if(lines >=1 )
+console.log(lines - 1);
