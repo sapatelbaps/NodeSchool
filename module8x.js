9718c0c35cd73cd1aceecf013ecf8fb0f74b0b2b
sapatelbaps <sapatelbaps@gmail.com>
2019-05-16 19:07:27 -0700

    Used online solution to fix the bl issue.

--- module8x.js
+++ module8x.js
@@ -1,27 +1,22 @@
 // Module 8 - http.get request. Print two lines - count and all data in second line.
-// Not working as expected because don't know when ondata will be completed!
-// node module8.js http://www.classical.net
+// node module8x.js http://www.classical.net
+// https://stackoverflow.com/questions/37899263/pipe-function-in-node-and-http-get
+
 http = require('http');
-var fullData = '';
+var bl = require('bl');
 
-http.get(process.argv[2],function callback (response) {
-    response.setEncoding('utf8');
-    //console.log("Got response: " + response.statusCode);
-    response.on('data',showData);
-    console.log('finished on data call.');
-    
-    fullData = fullData.replace('\n','');
-    console.log(fullData);
-    
-})
-.on('error', showError);
-
-function showData(data){
-    //console.log(data);
-    fullData = fullData + data;
-    console.log('inside');
-    //console.log(fullData.length);
-}
+http.get(process.argv[2], function (response) {
+    response.pipe(bl(function (err, data) {
+      if (err)
+        return console.error(err);
+      data = data.toString();
+      console.log(data.length);
+      console.log(data);
+      
+      // Replace new line with empty character
+      // console.log(data.toString().replace(/\n/g,''));
+    }));
+  }).on('error', showError);
 
 function showError(e) {
     console.log("Got error: " + e.message);
