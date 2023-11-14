23085c4e1413a4ee4f13a163b0b08a2f7ddcb7fc
sapatelbaps <sapatelbaps@gmail.com>
2023-11-13 20:26:14 -0800

    Tried to solve TDD development but for some reason it does not pass all tests.

--- jsbestpractices/changeHandler.js
+++ jsbestpractices/changeHandler.js
@@ -1,25 +1,51 @@
-var balanceManager = require('./balanceManager');
+var balanceManager = require("./balanceManager");
 
 module.exports = {
-
   getAmount: function (coinType) {
     // COINS:
     // [p]enny
     // [n]ickel
     // [d]ime
     // [q]uarter
-    switch (coinType) {
-      case "p":
-        return 1;
-      case "n":
-        return 5;
-      case "d":
-        return 10;
-      case "q":
-        return 25;
-      default:
-        throw new Error("Unrecognized coin " + coinType);
+    let coinValues = {
+      p: 1,
+      n: 5,
+      d: 10,
+      q: 25,
+    };
+
+    if (coinValues.hasOwnProperty(coinType)) {
+      return coinValues[coinType];
+    } else {
+      throw new Error("Unrecognized coin " + coinType);
     }
   },
 
+  convertToChange: function (amount) {
+    let coins = ["q", "d", "n", "p"];
+    let change = [];
+
+    for (coin of coins) {
+      let data = this.getDivider(amount, coin);
+      if (data.length) {
+        change = change.concat(data);
+        amount = parseInt(amount % this.getAmount(coin));
+      }
+    }
+    
+    return change.reverse();
+  },
+
+  getDivider: function (amount, coin) {
+    let divider = parseInt(amount / this.getAmount(coin));
+    let changeCoins = [];
+
+    if (divider >= 1) {
+      for (let i = 0; i < divider; i++) {
+        changeCoins.push(coin);
+      }
+    }
+
+    return changeCoins;
+  },
 };
---
+++ test/jsbestpractices.js
@@ -0,0 +1,5 @@
+const changeHandler = require('../jsbestpractices/changeHandler');
+
+console.log('89 => '+changeHandler.convertToChange(89));
+console.log('42 => '+changeHandler.convertToChange(42));
+console.log('76 => '+changeHandler.convertToChange(76));
